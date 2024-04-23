b60d6d9c39dc989c4fdadc068ee62625dd8f1b89
kariuki11 <irungukenneth433@gmail.com>
2024-04-23 11:36:56 -0700

    The new changes on this file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Am also specifying the version of flask to install using the ensure attribute
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
+# Install a package
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
